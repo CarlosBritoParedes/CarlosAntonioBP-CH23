62209b4a688ae7cd785a39e8e9e45e8e85d044d2
Carlos Brito <Ing.CarlosBritoParedes@gmail.com>
2023-02-22 14:24:54 -0600

    Archivos actualizados

---
+++ intro-web-7/js/Js-10/ProyectoTest/calculadora/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+    suma (a, b){
+        return a+b;
+    },
+    resta (a, b){
+        return a-b;
+    },
+    multy (a, b){
+        return a*b;
+    },
+    divide (a, b){
+        return a/b;
+    }
+}
