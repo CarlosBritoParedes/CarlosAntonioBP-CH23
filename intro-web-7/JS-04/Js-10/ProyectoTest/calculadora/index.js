5a5cabf0df56f5e49f270dba00d90a1286eb088b
Carlos Brito <Ing.CarlosBritoParedes@gmail.com>
2023-02-22 19:32:18 -0600

    Carpeta JS actualizada

---
+++ intro-web-7/JS-04/Js-10/ProyectoTest/calculadora/index.js
@@ -0,0 +1,17 @@
+//export
+
+// module.exports = {
+module.exports.indexTest = {
+  suma(a, b) {
+    return a + b;
+  },
+  resta(a, b) {
+    return a - b;
+  },
+  multi(a, b) {
+    return a * b;
+  },
+  div(a, b) {
+    return a / b;
+  },
+};
--- intro-web-7/js/Js-10/ProyectoTest/test/indexText.test.js
+++
@@ -1,7 +0,0 @@
-//prueba efectiva
-const indexTest = require ("../calculadora");
-test('test suma', () => {
-  const r = indexTest.suma(3,2);
-  expect (r).toBe(5);
-
-});
